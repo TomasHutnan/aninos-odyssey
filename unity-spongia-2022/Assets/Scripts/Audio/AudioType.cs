a0709d912c4f988bf2c708a700c9f1bf26bf1098
TomasHutnan <tomas.hutnan147@gmail.com>
2022-10-29 14:13:29 +0200

    Audio System

---
+++ unity-spongia-2022/Assets/Scripts/Audio/AudioType.cs
@@ -0,0 +1,41 @@
+﻿
+namespace AE.Audio
+{
+    public enum AudioType
+    {
+        None,
+
+        // Sound Tracks
+        //  - Game Location Themes
+        ST_LOC_GR_01,
+        ST_LOC_RO_01,
+        ST_LOC_EG_01,
+        ST_SHOP_01,
+
+        //  - Intro Scene
+        ST_INTRO_01,
+
+        //  - Fight Location Themes
+        ST_FIGHT_GR_NORMAL_01,
+        ST_FIGHT_GR_BOSS_01,
+
+        ST_FIGHT_RO_NORMAL_01,
+        ST_FIGHT_RO_BOSS_01,
+
+        ST_FIGHT_EG_NORMAL_01,
+        ST_FIGHT_EG_BOSS_01,
+
+        // Sound Effects
+        //  - UI
+        //    - Menu
+        SFX_MENU_BUTTON_HOVER_01,
+        SFX_MENU_BUTTON_PRESSED_01,
+
+        //  - Fight
+        SFX_FIGHT_HIT_NORMAL_01,
+        SFX_FIGHT_HIT_CRITICAL_01,
+        SFX_FIGHT_HIT_SPELL_01,
+
+        SFX_FIGHT_DODGE_01,
+    }
+}
