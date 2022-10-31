26acb2433e12f85a45019c13d87d2a05129bd2c3
dejvokep <davidcubesvkdev@gmail.com>
2022-10-31 17:05:00 +0100

    SaveData can now set defaults, save and load.

--- unity-spongia-2022/Assets/Scripts/GameSaving/SaveData.cs
+++ unity-spongia-2022/Assets/Scripts/GameSaving/SaveData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using AE.CharacterStats;
@@ -7,6 +8,7 @@ namespace AE.GameSave
 {
     public class SaveData
     {
+        private static SaveSlot slot = SaveSlot.None;
 
         public static int Money = 0;
         public static List<Item> Inventory = new List<Item>();
@@ -23,5 +25,66 @@ namespace AE.GameSave
         public static bool[] IsMuted = { false, false };
         // + stage, xp/amount of fights done in a stage?
 
+        public static void SetDefaults() {
+            slot = SaveSlot.None;
+            Money = 0;
+            Inventory = new List<Item>();
+            EquippedItems = new Dictionary<ItemType, Item>();
+            OwnedAbilities = new HashSet<int>();
+            EquippedAbilities = new HashSet<int>();
+            GameStage = ItemTier.Mortal;
+            ConfirmSell = true;
+            LevelUpSystem = new LevelUpSystem();
+            IsMuted = new bool[]{false, false};
+        }
+
+        public static bool hasSlot() {
+            return slot != SaveSlot.None;
+        }
+
+        public static void AutoSave() {
+            Save(SaveSlot.AutoSave);
+        }
+        public static void Save() {
+            if (slot == SaveSlot.None)
+                throw new ArgumentException("The current save does not have any slot associated!");
+
+            Save(slot);
+        }
+
+        public static void Save(SaveSlot slot) {
+            SaveData.slot = slot;
+            // call controller
+        }
+
+        public static void Load(JSONSave save) {
+            Money = save.Money;
+            
+            Inventory = new List<Item>(save.Inventory.Count);
+            foreach (JSONItem item in save.Inventory) {
+                Inventory.Add(new Item(
+                    item.Class, item.Tier, item.Type,
+                    item.Bonuses.Damage, item.Bonuses.CritPercent, item.Bonuses.Armor, item.Bonuses.Dodge, item.Bonuses.Mana,
+                    item.Weight, item.Name
+                ));
+            }
+
+            EquippedItems = new Dictionary<ItemType, Item>();
+            foreach (JSONItem item in save.EquippedItems) {
+                EquippedItems.Add(item.Type, new Item(
+                    item.Class, item.Tier, item.Type,
+                    item.Bonuses.Damage, item.Bonuses.CritPercent, item.Bonuses.Armor, item.Bonuses.Dodge, item.Bonuses.Mana,
+                    item.Weight, item.Name
+                ));
+            }
+
+            OwnedAbilities = new HashSet<int>(save.OwnedAbilities);
+            EquippedAbilities = new HashSet<int>(save.EquippedAbilities);
+            GameStage = save.GameStage;
+            ConfirmSell = save.ConfirmSell;
+            LevelUpSystem = new LevelUpSystem(null, save.LevelUpSystem.Level, save.LevelUpSystem.Exp,
+                save.LevelUpSystem.Bonuses.Damage, save.LevelUpSystem.Bonuses.CritChance, save.LevelUpSystem.Bonuses.Health, save.LevelUpSystem.Bonuses.Resistance, save.LevelUpSystem.Bonuses.DodgeChance, save.LevelUpSystem.Bonuses.Stamina, save.LevelUpSystem.Bonuses.Mana);
+            IsMuted = save.IsMuted.ToArray();
+        }
     }
 }
