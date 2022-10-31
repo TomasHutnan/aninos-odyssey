1877c14061631a8e4e82f3347e4a844803d1fd54
dejvokep <davidcubesvkdev@gmail.com>
2022-10-31 17:04:42 +0100

    Added SaveSlot enum, loader and utility methods.

--- unity-spongia-2022/Assets/Scripts/GameSaving/SaveController.cs
+++ unity-spongia-2022/Assets/Scripts/GameSaving/SaveController.cs
@@ -1,18 +1,52 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AE.Items;
+using Newtonsoft.Json;
 
-public class SaveController : MonoBehaviour
+namespace AE.GameSave
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public enum SaveSlot
     {
-        
+        None = -1, AutoSave = 0, Slot1 = 1, Slot2 = 2, Slot3 = 3
     }
 
-    // Update is called once per frame
-    void Update()
+    public class SaveController
     {
-        
+        static SaveController() {
+            string path = Path.Combine(Application.persistentDataPath, "no_topping_left_beef.json");
+            if (File.Exists(path)) {
+                StreamReader reader = new StreamReader(path);
+                data = JsonConvert.DeserializeObject<JSONBase>(reader.ReadToEnd());
+                reader.Close();
+            } else {
+                data = new JSONBase {
+                    AutoSave = null,
+                    Saves = new Dictionary<int, JSONSave>()
+                };
+            }
+        }
+
+        private static JSONBase data;
+
+        public static void StartNew() {
+            SaveData.SetDefaults();
+        }
+
+        public static void ActivateSave(SaveSlot slot) {
+            if (!isSlotOccupied(slot)) {
+                StartNew();
+                return;
+            }
+
+            SaveData.Load(data.Saves[(int) slot]);
+        }
+
+        public static bool isSlotOccupied(SaveSlot slot) {
+            return slot == SaveSlot.AutoSave ? data.AutoSave != null : data.Saves.ContainsKey((int) slot);
+        }
+
     }
 }
