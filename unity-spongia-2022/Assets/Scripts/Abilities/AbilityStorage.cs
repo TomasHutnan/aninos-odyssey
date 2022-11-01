17f260a1c0e2011ba972381d63a39486a5c7eb10
Carrrotka <90919351+Carrrotka@users.noreply.github.com>
2022-11-01 16:32:50 +0100

    Added Ability Names

--- unity-spongia-2022/Assets/Scripts/Abilities/AbilityStorage.cs
+++ unity-spongia-2022/Assets/Scripts/Abilities/AbilityStorage.cs
@@ -7,6 +7,56 @@ public class AbilityStorage : MonoBehaviour
     public enum AbilityName
     {
         None,
+        //Attacks
+        Armor_Break,
+        Fatigue,
+        Fighters_Attack,
+        Greater_Fighters_Attack,
+        Greater_Priest_Attack,
+        Greater_Rouge_Attack,
+        Greater_Tank_Attack,
+        Lesser_Attack,
+        Priest_Attack,
+        Rouge_Attack,
+        Stun_Attack,
+        Tank_Attack,
+        //Blessing
+        Greater_Crit_Chance_Blessing,
+        Greater_Damage_Blessing,
+        Greater_Defence_Blessing,
+        Greater_Dodge_Chance_Blessing,
+        Greater_Health_Blessing,
+        Lesser_Crit_Chance_Blessing,
+        Lesser_Damage_Blessing,
+        Lesser_Defence_Blessing,
+        Lesser_Dodge_Chance_Blessing,
+        Lesser_Health_Blessing,
+        Crit_Chance_Blessing,
+        Damage_Blessing,
+        Defence_Blessing,
+        Dodge_Chance_Blessing,
+        Health_Blessing,
+        //DefensiveBlessing
+        Greater_Fighters_Defence,
+        Greater_Priests_Defence,
+        Greater_Rouges_Defence,
+        Greater_Tanks_Defence,
+        Fighters_Defence,
+        Priests_Defence,
+        Rouges_Defence,
+        Tanks_Defence,
+        Lesser_Defence,
+        //GodAbilities
+        BloodLust,
+        Earthen_Shield,
+        Heavens_Blessing,
+        Total_Precision,
+
+
+
+
+
+
 
     }
     [System.Serializable]
