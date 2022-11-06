782966829ddc3dd0329d91f291315edc5e5131c7
TomasHutnan <tomas.hutnan147@gmail.com>
2022-11-06 04:06:15 +0100

    add menu sfx button press

---
+++ unity-spongia-2022/Assets/Scripts/Menu/SFXProvider.cs
@@ -0,0 +1,27 @@
+using AE.Audio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AudioType = AE.Audio.AudioType;
+
+namespace AE.MainMenu
+{
+    public class SFXProvider : MonoBehaviour
+    {
+        [SerializeField] AudioController audioController;
+        [Space]
+        [SerializeField] AudioType buttonHoverSFX;
+        [SerializeField] AudioType buttonPressedSFX;
+
+        public void OnButtonHover()
+        {
+            if (buttonHoverSFX != AudioType.None)
+                audioController.PlayAudio(buttonHoverSFX);
+        }
+        public void OnButtonPressed()
+        {
+            if (buttonPressedSFX != AudioType.None)
+                audioController.PlayAudio(buttonPressedSFX);
+        }
+    }
+}
